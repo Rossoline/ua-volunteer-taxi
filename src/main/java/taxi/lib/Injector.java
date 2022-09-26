dcb6c3e3534e999ce0ccb623605e501b2b636673
Ilja Nekrituk <Nekrit99@gmail.com>
2022-09-26 12:25:47 +0300

    Injector

--- src/main/java/taxi/lib/Injector.java
+++ src/main/java/taxi/lib/Injector.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Injector {
     private static final Map<String, Injector> injectors = new HashMap<>();
